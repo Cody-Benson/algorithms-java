a8dc8889c06417d7639f4c144d4f299597d77be3
cody <codyobenson@gmail.com>
2025-10-09 01:07:15 -0500

    added combinations

---
+++ sets/Combinations.java
@@ -0,0 +1,9 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Combinations {
+    public ArrayList<ArrayList<String>> calculateCombinations(ArrayList<String> letters){
+        
+
+    }
+}
---
+++ sets/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Main{
+    public static void main(String[] args){
+        ArrayList<String> input = new ArrayList<>();
+        input.add("a");
+        Combinations(input);
+    }
+}
